d31c6426ccb9da0a41bb103dde24d1f036d43bea
IshantNikure07 <ishantnikure712@gmail.com>
2025-05-09 14:24:00 +0530

    second commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,30 @@ import back from "../assets/back.png"
 
 const Home = () => {
   return (
-    <div className=' bg-[#6001BE] w-[100vw] h-[100vh]' >
+    <section className=' bg-[#6001BE] w-[100vw] h-[100vh]' >
         <div className='w-[100vw] h-[90%]' style={{ backgroundImage: `url(${back})` ,  backgroundRepeat: 'no-repeat',
           backgroundSize: 'cover', // or 'contain' based on your need
           backgroundPosition: 'center'}}>
-            
+
+            {/* Navbar */}
+            <div>
+                <img src="" alt="" />
+                <div>
+                   <h1>Service</h1>
+                   <h1>Hire Indian Talent</h1>
+                   <h1>Our Portfolio</h1>
+                   <h1>Blog</h1>
+                   <h1>Bool a Assighment</h1>
+                </div>
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
         </div>
-    </div>
+    </section>
   )
 }
 
